4ca2fd77f751a78b1064f7d04a89325e4b2fcd8b
Grant Mathews <grant@causes.com>
2011-07-30 00:20:03 -0700

    Ok. maybe this is a game idea?

--- src/minild28/core.clj
+++ src/minild28/core.clj
@@ -16,6 +16,7 @@
 
 (def *size* 500)
 (def *t-width* 20)
+(def output (atom []))
 
 (def keys-down (atom #{}))
 
@@ -74,42 +75,65 @@
       (let [ch (nth (nth map1 y) x)]
         (draw-tile x y ch gfx)))))
 
-(defn render-game [gfx player]
+
+(defn render-state [gfx state]
+  (doseq [guy (:guys state)]
+    (do
+      (.setColor gfx (java.awt.Color/BLACK))
+      (draw-tile (:x guy) (:y guy) \c gfx))))
+
+(defn render-game [gfx state]
   (.setColor gfx (java.awt.Color/BLACK))
 
   (.fillRect gfx 0 0 *size* *size*)
+
   (render-map gfx)
-
-  (.setColor gfx (java.awt.Color/WHITE))
-  (draw-tile (:x player) (:y player) \c gfx)
-
-  ;(.setColor (java.awt.Color/BLUE))
-  ;(.fillRect (* 10 (deref x)) (* 10 (deref y)) 10 10)
+  (render-state gfx state)
 )
 
-(defn drawRectangle [p player]
+(defn double-buffer-render [p state]
   (let [bfs (.getBufferStrategy p)
         gfx (.getDrawGraphics bfs)]
-    (render-game gfx player)
+    (render-game gfx state)
     (.show bfs)
     (.sync (Toolkit/getDefaultToolkit))
 ))
 
-(defn update-player [player]
-  (let [old-x (:x player)
-        old-y (:y player)
-        
-        new-x 2
-        ]
-    player))
+
+(defn key-down? [x]
+  (if (@keys-down x) 1 0))
+
+;(defn update-player [player]
+;  (let [old-x (:x player)
+;        old-y (:y player)
+;        new-x (+ old-x (key-down? 68) (- (key-down? 65)))
+;        new-y (+ old-y (key-down? 83) (- (key-down? 87)))]
+;    {:x new-x :y new-y}))
+
+(defn update-turn [turn]
+  turn)
+
+(defn update-guys [guys]
+  guys)
+
+(defn update-game-state [state]
+  {:guys (update-guys (:guys state))
+   :turn (update-turn (:turn state))})
 
 (defn game [frame]
-  (loop [player {:x 3 :y 3}]
-    (drawRectangle frame player)
-    (Thread/sleep 5)
-    (.setTitle frame "WAR.")
-    (recur (update-player player))
-    ))
+         ; This is the starting state of the entire game.
+         ; At first I thought it was weird, but then I realized I kinda like 
+         ; how Clojure almost requires you to group all state together...
+  (loop [state { :guys [{:type :guy :x 8    :y 8}
+                        {:type :guy :x 10   :y 8}
+                        {:type :badguy :x 1 :y 2}]
+                 :turn :yours
+                }]
+  (println "Hi")
+    (double-buffer-render frame state)
+    (Thread/sleep 15)
+    (.setTitle frame (str @keys-down))
+    (recur (update-game-state state))))
 
 (defn -main[]
   (def panel
@@ -138,7 +162,10 @@
       ; Why does this line not work?
       (.createBufferStrategy 2)
       (.setVisible true))
-
-  (future (game frame)))
+  
+  (println "sup")
+  (def f (future-call (bound-fn [] (game frame))))
+  
+ ) 
 
 (-main)
